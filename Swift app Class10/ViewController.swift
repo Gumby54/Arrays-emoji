6703a6c37ccacac92b59617b1c3abaee3436f917
Gilles Prescott <gprescott1@macbook-pro.local>
2017-01-07 11:28:20 -0500

    Initial Commit

---
+++ Swift app Class10/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift app Class10
+//
+//  Created by Gilles Prescott on 2017-01-07.
+//  Copyright © 2017 Gilles Prescott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
